8c103bd0011013fc370fb7f465dc669d071e3dc0
lijia <lijia0732@sina.com>
2024-10-04 09:46:15 +0800

    fix: 解决dns记录越来越多的问题

--- index.js
+++ index.js
@@ -21,14 +21,26 @@ async function handleRequest(request) {
       
       const allResultsFutrue = allTasks.map(async task => {
           const headers = { "Content-Type" : "application/json", Authorization: `Bearer ${task.api_bear_token}`}
-          const data = { type:"A",name : task.domain, "content": ip,"proxied":false }
-          const response = await fetch(`https://api.cloudflare.com/client/v4/zones/${task.zone_id}/dns_records`, {
-              method: 'POST',
-              headers,
-              body: JSON.stringify(data),
-          });
-          const result = await response.json();
-          return result
+          
+          // 先查询有没有A记录
+          const queryRes = await fetch(`https://api.cloudflare.com/client/v4/zones/${task.zone_id}/dns_records?name=${task.domain}&type=A,CNAME`, { headers });
+          const queryResult = await queryRes.json();
+          // 如果含有记录，且记录大于1
+          const records = queryResult.result
+          const rlength = records.length 
+          if(rlength > 1){
+              // 删除记录，仅剩最后一个
+              const rDelete = await Promise.all( records.slice(0, rlength-1).map( record => deleteA(task, record, headers)  ) )
+              // console.log("delete result: ", rDelete)
+              // 更新记录
+              return updateA(task, ip, headers, records[rlength-1])
+          } else if(rlength == 1){
+              //console.log("update record only")
+              return updateA(task, ip, headers, records[0])
+          } else {
+              //console.log("create record only", rlength)
+              return createA(task, ip, headers)
+          }
       })
       const r = await Promise.all(allResultsFutrue);
       return new Response(JSON.stringify(r), { status: 200, headers: { "Content-Type": "application/json" } });
@@ -38,6 +50,30 @@ async function handleRequest(request) {
 
 }
 
+async function deleteA(task, oldRecord, headers){
+    const response = await fetch(`https://api.cloudflare.com/client/v4/zones/${task.zone_id}/dns_records/${oldRecord.id}`, { method: 'DELETE', headers, });
+    return response.json();
+}
+
+async function updateA(task, ip, headers, oldRecord){
+    const data = { ...oldRecord, type:"A",name : task.domain, "content": ip,"proxied":false }
+    const response = await fetch(`https://api.cloudflare.com/client/v4/zones/${task.zone_id}/dns_records/${oldRecord.id}`, {
+      method: 'PATCH',
+      headers,
+      body: JSON.stringify(data),
+    });
+    return response.json();
+}
+
+async function createA(task, ip, headers){
+    const data = { type:"A",name : task.domain, "content": ip,"proxied":false }
+    const response = await fetch(`https://api.cloudflare.com/client/v4/zones/${task.zone_id}/dns_records`, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(data),
+    });
+    return response.json();
+}
 
 async function getGcoreIP(){
     return new Promise(async (resolve, reject) => {
